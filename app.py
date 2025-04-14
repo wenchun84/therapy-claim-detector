bb339876a3339cfc76dc6cc8b2c114ef65f0fa2c
wenchun84 <linwenchun@gmail.com>
2025-04-14 19:50:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template
+cat > app.py << 'EOL'
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='首頁')
-
-@app.route('/about')
-def about():
-    return render_template('about.html', title='關於')
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
+EOL
