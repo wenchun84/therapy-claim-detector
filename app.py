0354a6054574b3437fa0073f2f5dd131d666e5e3
wenchun84 <linwenchun@gmail.com>
2025-04-14 19:22:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='首頁')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='關於')
 
 if __name__ == '__main__':
     app.run(debug=True)
